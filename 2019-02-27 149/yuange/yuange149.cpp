6077ebb6c7c2d7497a1aef76519b9a941cf4a589
Greenson Gavin <greensongavin@GreensondeMacBook-Pro-2.local>
2019-02-28 00:37:17 +0800

    update yuange149

--- 2019-02-27 149/yuange/yuange149.cpp
+++ 2019-02-27 149/yuange/yuange149.cpp
@@ -8,9 +8,20 @@
  * };
  */
 #define INF 0x3f3f3f3f
-#include <unordered_map>
 #include <osstream>
 class Solution {
+private:
+    struct  Test
+    {
+        int x;
+        int y;
+        
+        Test(int a, int b) : x(a), y(b) {}
+        bool operator < (const Test &o) const
+        {
+            return x < o.x || (x == o.x && y < o.y);
+        }
+    };
 public:
     int maxPoints(vector<Point> &points) {
         int n = points.size();
@@ -18,17 +29,17 @@ public:
         int maxNum=0;
         int same=0;
         for (int i = 0; i < n; i++) {
-            unordered_map<string,int> pointLine;
+            map<Test,int> pointLine;
             same=0;
             for (int j = 0; j < i; j++) {
                 if(j == i) continue;
                 int x1 = points[i].x, y1 = points[i].y, x2 = points[j].x, y2 = points[j].y;
                 int dx = x1 - x2, dy = y1 - y2;
                 if(dx == 0 && dy == 0) same++; // 重复点
-                else if(dx == 0) pointLine[i2str(INF, 0)]++; // 平行于y轴
+                else if(dx == 0) pointLine[Test(INF, 0)]++; // 平行于y轴
                 else { // 常规点
                     int g = gcd(dx, dy);
-                    pointLine[i2str(dy/g, dx/g)]++;
+                    pointLine[Test(dy/g, dx/g)]++;
                 }
             }
             int lineMax=0;
@@ -44,13 +55,5 @@ public:
     {
         if (b == 0) return a;
         else return gcd(b, a % b);
-    }
-    
-    // 斜率k=k1/k2
-    inline string i2str(int k1, int k2)
-    {
-        ostringstream oss;
-        oss << k1 << '-' << k2;
-        return oss.str();
     }
 };
