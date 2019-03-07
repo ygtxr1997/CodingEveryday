d1b4fe2589133a9fa644ccc6fe21c4c1d93df347
lovelydragon <747416462@qq.com>
2019-03-07 10:55:48 +0800

    Create 88.java

---
+++ Year-2019/Mar/2019-03-07-88/lb/88.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //从尾部从大往小排序
+        for(int i = m+n-1; i>=0;i--){
+        //整合mn为0的边界条件
+            if(n == 0 || ( m != 0 && nums1[m-1]>= nums2[n-1]) ){
+                nums1[i] = nums1[m-1];
+                m--;
+            }else{
+                nums1[i] = nums2[n-1];
+                n--;
+            }
+        }
+    }
+}
