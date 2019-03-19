dda7c0a7dad2cc8b99c2f277afef9be6ae1ef91b
lovelydragon <747416462@qq.com>
2019-03-19 15:58:50 +0800

    Update 334worse.java

--- Year-2019/Mar/2019-03-19-334/lb/334worse.java
+++ Year-2019/Mar/2019-03-19-334/lb/334worse.java
@@ -1,5 +1,6 @@
 class Solution {
 //通过栈来保存前两位递增的情况，且实时变更前两位更小的最优的状态，当出现第三个更大的数时则满足要求。
+    //其实没有必要使用栈，可以直接使用两个参数来记录最小的第一位和第二位数
     public boolean increasingTriplet(int[] nums) {
        int min = Integer.MAX_VALUE;
         Stack<Integer> stack = new Stack<>();
