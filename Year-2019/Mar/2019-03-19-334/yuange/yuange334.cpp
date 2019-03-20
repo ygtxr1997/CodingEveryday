fd26b404aeedabe51d7406ae292df71864a71dc2
apple <apple@192.168.3.21>
2019-03-20 22:20:32 +0800

    update yuange334

--- Year-2019/Mar/2019-03-19-334/yuange/yuange334.cpp
+++ Year-2019/Mar/2019-03-19-334/yuange/yuange334.cpp
@@ -1,5 +1,5 @@
-// 非常神奇的思路，好好体会
-// 把数组从左到右分成几个部分：比mi大的---mi---大于mi且小于ma的---ma---待定
+// 非常神奇的思路，好好体会，还要注意等号的问题
+// 把数组从左到右分成几个部分：比mi大的---mi---比ma大的---ma---待定，mi<ma
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
