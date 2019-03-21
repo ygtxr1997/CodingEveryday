9ddbd41db623ad637872e4c068e1ac41827265b2
vegedog <1429523226@qq.com>
2019-03-21 10:26:03 +0800

    Create MinStack.java

---
+++ Year-2019/Mar/2019-03-21-155/Immanuel/MinStack.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Arrays;
+
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    boolean flag = false;
+    int incre = 10;
+    int size = 100;
+    int[] stack;
+    int top = 0;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new int[size];
+    }
+
+    public void push(int x) {
+        if(top >= size){
+            stack = Arrays.copyOf(stack, size += incre);
+        }
+
+        stack[top++] = x;
+        if(x < min){
+            min = x;
+            flag = true;
+        }
+    }
+
+    public void pop() {
+        top--;
+        if(stack[top] == min){
+            flag = false;
+        }
+    }
+
+    public int top() {
+        return stack[top - 1];
+    }
+
+    public int getMin() {
+        if(flag)
+            return min;
+        else{
+            min = Integer.MAX_VALUE;
+            for(int i = 0; i < top; i++){
+                min = Math.min(min, stack[i]);
+            }
+            flag = true;
+            return min;
+        }
+    }
+}
