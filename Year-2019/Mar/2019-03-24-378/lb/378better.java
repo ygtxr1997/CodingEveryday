5eab4b9cba2e732ad99e7022d5a941799fc152b5
lovelydragon <747416462@qq.com>
2019-03-27 16:23:36 +0800

    Update 378better.java

--- Year-2019/Mar/2019-03-24-378/lb/378better.java
+++ Year-2019/Mar/2019-03-24-378/lb/378better.java
@@ -1,4 +1,5 @@
 class Solution {
+//在中间数位置（getmid函数的作用），比较来判定取哪一边继续寻找
 //二维数组的二分查找法
     public int kthSmallest(int[][] matrix, int k) {
      int n=matrix.length;
@@ -15,18 +16,19 @@ class Solution {
         }
         return lo;
     }
-    //
- public int getmid(int[][] matrix,int mid){
-     int n=matrix.length;
-     int res=0;
-     for(int i=n-1,j=0;i>=0&&j<n;){
-         if(matrix[i][j]>mid)
-             i--;
-         else{
-             res+=i+1;
-             j++;
-         }       
-     }
-     return res;
- }   
+    //返回mid在数组中排序
+    public int getmid(int[][] matrix,int mid){
+        int n=matrix.length;
+        int res=0;
+        //根据数组的双向有序，
+        for(int i=n-1,j=0;i>=0&&j<n;){
+            if(matrix[i][j]>mid)
+                i--;
+            else{
+                res+=i+1;//记录当前列有多少个数比mid小
+                j++;//换下一行，不存在matrix[i+1][j++]<mid的情况，所以放心换
+            }
+        }
+        return res;
+    }
 }
