4ae7842ca1285662846be03d870aea7572e5a51b
vegedog <1429523226@qq.com>
2019-04-16 15:46:01 +0800

    update zwh382.java

--- Year-2019/Mar/2019-03-17-384/Immanuel/zwh384.java
+++ Year-2019/Mar/2019-03-17-384/Immanuel/zwh384.java
@@ -16,10 +16,25 @@ public class Solution {
     public int[] shuffle() {
         if(nums == null) return null;
         int[] a = nums.clone();
-        for(int j = 1; j < a.length; j++) {
+        // for(int j = 0; j < a.length; j++) {
+        //     int i = random.nextInt(a.length);
+        //     swap(a, i, j);
+        // }
+        
+        //第j个数字有1/j的概率被留下，从而保证每个数字出现在每个位置的概率相同
+        // for(int j = 1; j < a.length; j++) {
+        //     int i = random.nextInt(j + 1);
+        //     swap(a, i, j);
+        // }
+        //对于第i个元素，在洗牌后，其在n-1位置上的概率是1/n，其在第n-2位置上的概率为(n-1)/n * 1/(n-1)，
+        //即第一次没有被选中到n-1位置上，第2次被选中了，到了n-2位置上，
+        //相应的，第i个元素在n-j位置上的概率为(n-1)/n * (n-2)/(n-1) * ... * (n-j+1)/(n-j+2) * 1/(n-j+1)，为1/n 
+        for(int j = a.length - 1; j >= 1; j--) {
             int i = random.nextInt(j + 1);
             swap(a, i, j);
         }
+        
+        
         return a;
     }
     
