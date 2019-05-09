475add039950ed2f4a709c5169a526009b0c3e0d
vegedog <1429523226@qq.com>
2019-05-09 17:50:43 +0800

    update zwh236.java

--- Year-2019/Apr/2019-04-08-236/Immanuel/zwh236.java
+++ Year-2019/Apr/2019-04-08-236/Immanuel/zwh236.java
@@ -1,37 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
 public class Solution {
-    private int lOrR;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
-            lOrR = 0;
-            return null;
-        }
-        
-        int tmp = 0;
-        if(root == p){
-            tmp = 1;
-        }
-        else if(root == q){
-            tmp = 2;
-        }
-        
-        TreeNode tmp1 = lowestCommonAncestor(root.left, p, q);
-        if(tmp1 != null)
-            return tmp1;
-        if(lOrR != tmp){
-            tmp += lOrR;
-        }
-        tmp1 = lowestCommonAncestor(root.right, p, q);
-        if(tmp1 != null)
-            return tmp1;
-        if(lOrR != tmp){
-            tmp += lOrR;
-        }
-        
-        if(tmp == 3)
-            return root;
-        else {
-            lOrR = tmp;
-            return null;
-        }
+        if(root == null || root == p || root == q)  return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null)   return root;
+        return left != null ? left : right;
     }
 }
+
+
+// public class Solution {
+//     private int lOrR;
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null){
+//             lOrR = 0;
+//             return null;
+//         }
+        
+//         int tmp = 0;
+//         if(root == p){
+//             tmp = 1;
+//         }
+//         else if(root == q){
+//             tmp = 2;
+//         }
+        
+//         TreeNode tmp1 = lowestCommonAncestor(root.left, p, q);
+//         if(tmp1 != null)
+//             return tmp1;
+//         if(lOrR != tmp){
+//             tmp += lOrR;
+//         }
+//         tmp1 = lowestCommonAncestor(root.right, p, q);
+//         if(tmp1 != null)
+//             return tmp1;
+//         if(lOrR != tmp){
+//             tmp += lOrR;
+//         }
+        
+//         if(tmp == 3)
+//             return root;
+//         else {
+//             lOrR = tmp;
+//             return null;
+//         }
+//     }
+// }
