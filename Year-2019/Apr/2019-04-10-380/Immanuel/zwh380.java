cf2c62f2e26021f76142c150a141a4fc936bf5d2
vegedog <1429523226@qq.com>
2019-05-10 19:11:39 +0800

    create zwh380.java

---
+++ Year-2019/Apr/2019-04-10-380/Immanuel/zwh380.java
@@ -0,0 +1,47 @@
+class RandomizedSet {
+    List<Integer> list;
+    Map<Integer, Integer> map;
+    Random random;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(map.containsKey(val))
+            return false;
+        
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val))
+            return false;
+        
+        map.put(list.get(list.size() - 1), map.get(val));
+        list.set(map.get(val), list.get(list.size() - 1));
+        map.remove(val);
+        list.remove(list.size() - 1);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
