2a6df622889a39d0cd536ef8c358827faa169163
vegedog <1429523226@qq.com>
2019-05-15 11:34:38 +0800

    create zwh162.java

---
+++ Year-2019/Apr/2019-04-15-162/Immanuel/zwh162.java
@@ -0,0 +1,10 @@
+public class Solution{
+    public int findPeakElement(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] > nums[i + 1])
+                return i;
+        }
+            
+        return nums.length - 1;
+    }
+}
