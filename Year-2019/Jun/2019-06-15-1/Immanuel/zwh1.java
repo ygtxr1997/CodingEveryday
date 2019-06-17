665c7263c5408b43ad05f804d0c99fcff6902f37
vegedog <1429523226@qq.com>
2019-06-17 11:46:03 +0800

    create zwh1.java

---
+++ Year-2019/Jun/2019-06-15-1/Immanuel/zwh1.java
@@ -0,0 +1,25 @@
+public class Solution {
+    //用HashMap进行加速
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[] {map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[] {-1, -1};
+    }
+
+//    public int[] twoSum(int[] nums, int target) {
+//        for(int i = 0; i < nums.length; i++){
+//            for(int j = i + 1; j < nums.length; j++){
+//                if(nums[i] + nums[j] == target)
+//                    return new int[] {i, j};
+//            }
+//        }
+//
+//        return new int[] {-1, -1};
+//    }
+}
