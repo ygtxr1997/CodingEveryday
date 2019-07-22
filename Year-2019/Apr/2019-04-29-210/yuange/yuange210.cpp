489ceb6f56707585fb7a9132362e15e6315b62a6
ygtxr1997 <gavingreenson@icloud.com>
2019-07-22 14:20:08 +0800

    add yuange210

---
+++ Year-2019/Apr/2019-04-29-210/yuange/yuange210.cpp
@@ -0,0 +1,30 @@
+// dfs，返回的序列是拓扑排序的逆序，24ms，81%
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        visited.resize(numCourses);
+        table.resize(numCourses);
+        for (auto v : prerequisites) table[v[1]].insert(v[0]);
+        for (int i = 0; i < numCourses; ++i) dfs(i);
+        return hasCircle ? vector<int>() : vector<int>(ans.rbegin(), ans.rend());
+    }
+    
+    void dfs(int cur) {
+        if (visited[cur] == 1) ;
+        else if (visited[cur] == 2) hasCircle = true;
+        else if (table[cur].empty()) {
+            ans.push_back(cur); // 叶子节点
+            visited[cur] = 1;
+        } else {
+            visited[cur] = 2;
+            for (auto son : table[cur]) dfs(son);
+            ans.push_back(cur);
+            visited[cur] = 1;
+        }
+    }
+private:
+    vector<unordered_set<int>> table;   // 存储子节点集合
+    vector<int> visited;                // 访问状态，0:未被访问，1:已被访问，2:正在访问
+    bool hasCircle;                     // 是否有环
+    vector<int> ans;                    // 上课顺序
+};
