62a24a5684f2e250a24536858cf76fa4af50dbff
lovelydragon <747416462@qq.com>
2019-07-30 13:05:44 +0800

    Create 557.java

---
+++ Year-2019/Jun/2019-06-22-557/lb/557.java
@@ -0,0 +1,37 @@
+/***
+ * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+ * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+ * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+ * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+ * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+ * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+ * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+ * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+ * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+ * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+ * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+ * 单身狗就这样默默地看着你，一句话也不说。
+ */
+
+/**
+ * @author ：lovelydragon
+ * @date ：Created in 2019-05-07 23:16
+ * @description：
+ */
+public class ReverseWords {
+
+    //反转字符串中的单词
+    //分割出单词，并对每个单词反转，最后重新组合
+    public String reverseWords(String s) {
+        StringBuffer result = new StringBuffer();
+        StringBuffer temp;
+        String[] strings = s.split(" ");
+        for (int i = 0; i < strings.length; i++) {
+            temp = new StringBuffer(strings[i]);
+            temp.reverse();
+            result.append(temp+" ");
+        }
+        result.delete(result.length()-1,result.length());
+        return result.toString();
+    }
+}
