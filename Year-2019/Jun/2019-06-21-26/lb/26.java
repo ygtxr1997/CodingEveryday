aad46a44296dbbd524327739009c57acd8097150
lovelydragon <747416462@qq.com>
2019-07-30 12:33:14 +0800

    Create 26.java

---
+++ Year-2019/Jun/2019-06-21-26/lb/26.java
@@ -0,0 +1,41 @@
+/***
+ * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+ * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+ * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+ * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+ * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+ * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+ * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+ * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+ * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+ * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+ * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+ * 单身狗就这样默默地看着你，一句话也不说。
+ */
+
+import org.omg.CORBA.INTERNAL;
+
+import java.util.Stack;
+
+/**
+ * @author ：lovelydragon
+ * @date ：Created in 2019-05-19 10:40
+ * @description：
+ */
+    //删除排序数组中的重复项
+    //双指针循环
+    public int removeDuplicates(int[] nums) {
+        int result = 0;
+        int left = 0;
+        int lastNum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (lastNum != nums[i]){
+                result++;
+                lastNum = nums[i];
+                nums[left] = nums[i];
+                left++;
+            }
+        }
+        return result;
+    }
+}
