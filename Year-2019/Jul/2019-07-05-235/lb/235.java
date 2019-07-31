e8e26bf94fee69fe31c22291b1057bd0476d97bc
lovelydragon <747416462@qq.com>
2019-07-31 19:56:29 +0800

    Create 235.java

---
+++ Year-2019/Jul/2019-07-05-235/lb/235.java
@@ -0,0 +1,36 @@
+/***
+ * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+ * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+ * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+ * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+ * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+ * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+ * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+ * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+ * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+ * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+ * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+ * 单身狗就这样默默地看着你，一句话也不说。
+ */
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author ：lovelydragon
+ * @date ：Created in 2019-04-16 20:01
+ * @description：
+ */
+public class LowestCommonAncestor {
+   
+    //二叉搜索树的最近公共祖先
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor2(root.right,p,q);
+        }else if (root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor2(root.left,p,q);
+        }else
+            return root;
+    }
+}
