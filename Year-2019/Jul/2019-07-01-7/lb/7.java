155f8340791b09e7df89af04b71792696a29013f
lovelydragon <747416462@qq.com>
2019-07-31 11:24:02 +0800

    Create 7.java

---
+++ Year-2019/Jul/2019-07-01-7/lb/7.java
@@ -0,0 +1,43 @@
+/***
+ * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+ * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+ * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+ * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+ * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+ * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+ * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+ * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+ * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+ * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+ * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+ * 单身狗就这样默默地看着你，一句话也不说。
+ */
+
+/**
+ * @author ：lovelydragon
+ * @date ：Created in 2019-07-31 10:28
+ * @description：
+ */
+public class Reverse {
+    //判断正负
+    //溢出判断--通过异常判断
+    //使用stringBuffer的reverse
+    public int reverse(int x) {
+        boolean plus = true;
+        if (x < 0){
+            x = -x;
+            plus = false;
+        }
+        StringBuffer stringBuffer = new StringBuffer(String.valueOf(x));
+        stringBuffer.reverse();
+        if (!plus){
+            stringBuffer = new StringBuffer("-"+stringBuffer);
+        }
+        try {
+            int result = Integer.parseInt(stringBuffer.toString());
+            return result;
+        }catch (NumberFormatException e){
+            return 0;
+        }
+    }
+}
