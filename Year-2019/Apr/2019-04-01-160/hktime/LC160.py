6cc6c7895ec191f32a175ef877e12981db6fb37f
hktime <hktime@users.noreply.github.com>
2019-09-09 13:53:21 +0800

    LC160

---
+++ Year-2019/Apr/2019-04-01-160/hktime/LC160.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Author: HK
+# @Date:   2019-09-09 13:52:05
+# @Last Modified by:   HK
+# @Last Modified time: 2019-09-09 13:52:38
+
+#
+# Be water, my friend.
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        # 关键在于长短链表长度差
+        if (headA == None or headB == None):
+            return None
+        pA, pB = headA, headB
+        while (pA != pB):
+            pA = pA.next if pA else headB
+            pB = pB.next if pB else headA
+        return pA
