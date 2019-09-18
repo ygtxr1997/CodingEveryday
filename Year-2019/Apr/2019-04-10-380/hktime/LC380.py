27b27568764b028f573c28b8bbe572720c64f3bc
hktime <hktime@users.noreply.github.com>
2019-09-18 09:31:04 +0800

    Create LC380.py

---
+++ Year-2019/Apr/2019-04-10-380/hktime/LC380.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# @Author: HK
+# @Date:   2019-09-18 09:30:23
+# @Last Modified by:   HK
+# @Last Modified time: 2019-09-18 09:30:30
+
+
+from random import choice
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.set = set()
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.set:
+            return False
+        self.set.add(val)
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.set:
+            self.set.remove(val)
+            return True
+        else:
+            return False
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        if not self.set:
+            return None
+        return random.choice(list(self.set))
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
