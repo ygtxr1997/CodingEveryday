67641d5a7e5b5319f144be1532160e977ab5b66a
yuange <gavingreenson@icloud.com>
2020-05-03 16:30:12 +0800

    add yuange53

---
+++ Year-2020/05-May/2020-05-03-53/yuange/yuange53.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = -0x7fffffff;
+        int sum = 0;
+        for (auto num : nums) {
+            sum += num;
+            res = max(res, sum);
+            if (sum <= 0) sum = 0;
+        }
+        return res;
+    }
+};
