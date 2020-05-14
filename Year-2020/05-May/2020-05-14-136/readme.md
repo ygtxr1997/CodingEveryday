187fde61c811f0b5c8ddec64ae98c0da40818859
yuange <gavingreenson@icloud.com>
2020-05-14 10:31:22 +0800

    add yuange136

---
+++ Year-2020/05-May/2020-05-14-136/readme.md
@@ -0,0 +1,2 @@
+## Q136 只出现一次的数字
+* [Q136](https://leetcode-cn.com/problems/single-number/)
---
+++ Year-2020/05-May/2020-05-14-136/yuange/yuange136.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); ++i) ans ^= nums[i];
+        return ans;
+    }
+};
