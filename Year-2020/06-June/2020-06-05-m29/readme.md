35b91be86ca3bcb26e7c93cb672d8d2b39b7ff1a
yuange <gavingreenson@icloud.com>
2020-06-05 11:24:35 +0800

    add yuangem29

---
+++ Year-2020/06-June/2020-06-05-m29/readme.md
@@ -0,0 +1,2 @@
+## 面试题29 顺时针打印矩阵
+* [面试题29](https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/)
---
+++ Year-2020/06-June/2020-06-05-m29/yuange/yuangem29.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return vector<int>();
+        int l = 0, u = 0, r = matrix[0].size() - 1, d = matrix.size() - 1;
+        vector<int> ret;
+        do {
+            for (int j = l; j <= r; j++) ret.push_back(matrix[u][j]);
+            u++; if (u > d) return ret;
+            for (int i = u; i <= d; i++) ret.push_back(matrix[i][r]);
+            r--; if (l > r) return ret;
+            for (int j = r; j >= l; j--) ret.push_back(matrix[d][j]);
+            d--;
+            for (int i = d; i >= u; i--) ret.push_back(matrix[i][l]);
+            l++;
+        } while (l <= r && u <= d);
+        return ret;
+    }
+};
